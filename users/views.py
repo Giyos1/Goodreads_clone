44080b44a3e241e4002166c1546b10e8eb181921
Giyos <giyosoripov4@gmail.com>
2022-04-11 22:47:08 +0500

    4-registratsiya

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views import View
 
 
@@ -9,6 +10,27 @@ class RegisterView(View):
     def get(self, request):
         return render(request, 'users/register.html')
 
+    def post(self, request):
+        # create user account
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.create(
+            username=username,
+            first_name=first_name,
+            last_name = last_name,
+            email=email,
+        )
+
+        user.set_password(password)
+        user.save()
+
+        return redirect('users:login')
+
+
 class LoginView(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'users/login.html')
